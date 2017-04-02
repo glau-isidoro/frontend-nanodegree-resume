285b0c849c903a970a5b6ab9ea40afff5114404c
glau-isidoro <glaucia.isidoro@gmail.com>
2017-04-02 00:16:51 -0300

    revisao udacity

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,5 @@
+var data = "%data%";
+
 var bio = {
   "name" : "Gláucia Isidoro",
   "role" : "Web Developer",
@@ -8,21 +10,22 @@ var bio = {
     "github" : "glau-isidoro",
     "location" : "São Paulo"
     },
-  "welcomeMessage" : "Cat ipsum dolor sit amet, if it smells like fish eat as much as you wish",
+  "welcomeMessage" : "Cat ipsum dolor sit amet, if it smells like fish eat" +
+  " as much as you wish",
   "skills" : [
     "awesomeness", "programming", "cryogenic sleep", "Java"
     ],
   "bioPic" : "images/glaucia-e-gato.jpg",
   display : function() {
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedName = HTMLheaderName.replace(data, bio.name);
+    var formattedRole = HTMLheaderRole.replace(data, bio.role);
+    var formattedBioPic = HTMLbioPic.replace(data, bio.bioPic);
+    var formattedMobile = HTMLmobile.replace(data, bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace(data, bio.contacts.email);
     var formattedLinkedin = HTMLlinkedin.replace(/%data%/g, bio.contacts.linkedin);
     var formattedGithub = HTMLgithub.replace(/%data%/g, bio.contacts.github);
-    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    var formattedLocation = HTMLlocation.replace(data, bio.contacts.location);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace(data, bio.welcomeMessage);
 
     $("#header").prepend(formattedRole);
     $("#header").prepend(formattedName);
@@ -38,18 +41,13 @@ var bio = {
     $("#bottomContacts").append(formattedLinkedin);
     $("#bottomContacts").append(formattedGithub);
 
-
     if(bio.skills.length > 0) {
       $("#header").append(HTMLskillsStart);
 
-      var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-      $("#skills").append(formattedSkill);
-      formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-      $("#skills").append(formattedSkill);
-      formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-      $("#skills").append(formattedSkill);
-      formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-      $("#skills").append(formattedSkill);
+      for (var i = 0; i < bio.skills.length; i++) {
+        var formattedSkill = HTMLskills.replace(data, bio.skills[i]);
+        $("#skills").append(formattedSkill);
+      }
     };
   }
 };
@@ -60,23 +58,30 @@ var work = {
     "employer" : "Metasix",
     "title" : "Programadora de Sistemas",
     "dates" : "Julho 2016 - atualmente",
-    "description" : "Cat ipsum dolor sit amet, then cats take over the world poop on grasses. See owner, run in terror and sometimes switches in french and say miaou just because well why not flop over cat is love, cat is life. Russian blue kitty scratches couch bad kitty sit and stare ignore the squirrels, you'll never catch them anyway, chase imaginary bugs.",
+    "description" : "Cat ipsum dolor sit amet, then cats take over the world" +
+                    " poop on grasses. See owner, run in terror and sometimes" +
+                    " switches in french and say miaou just because well why" +
+                    " not flop over cat is love, cat is life. Russian blue kitty" +
+                    " scratches couch bad kitty sit and stare ignore the squirrels," +
+                    " you'll never catch them anyway, chase imaginary bugs.",
     "url" : "http://www.metasix.com.br/",
     }
   ],
   display : function() {
-    for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    for (var i = 0; i < work.jobs.length; i++) {
       $("#workExperience").append(HTMLworkStart);
 
-      var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer).replace("%url%", work.jobs[job].url);
-      var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+      var formattedEmployer = HTMLworkEmployer.replace(data, work.jobs[i].employer)
+                              .replace("%url%", work.jobs[i].url);
+      var formattedTitle = HTMLworkTitle.replace(data, work.jobs[i].title);
       var formattedEmployerTitle = formattedEmployer + formattedTitle;
       $(".work-entry:last").append(formattedEmployerTitle);
 
-      var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+      var formattedDates = HTMLworkDates.replace(data, work.jobs[i].dates);
       $(".work-entry:last").append(formattedDates);
 
-      var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+      var formattedDescription = HTMLworkDescription.replace(data, work.jobs[i].description);
       $(".work-entry:last").append(formattedDescription);
     }
   }
@@ -89,7 +94,8 @@ var education = {
       "city" : "São Paulo, SP, BR",
       "degree" : "profissionalizante",
       "dates" : 2016,
-      "major" : "Formação Java - Java e Orientação a Objetos / Java para Desenvolvimento Web / Laboratório Java com Testes, JSF e Design Patterns.",
+      "major" : "Formação Java - Java e Orientação a Objetos / Java para " +
+                "Desenvolvimento Web / Laboratório Java com Testes, JSF e Design Patterns.",
       "url" : "https://www.caelum.com.br/cursos-java/",
     },
     {
@@ -110,31 +116,33 @@ var education = {
     }
   ],
   display : function() {
-    for (school in education.schools) {
+    for (var i = 0; i < education.schools.length; i++) {
       $("#education").append(HTMLschoolStart);
 
-      var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name).replace("%url%", education.schools[school].url);
+      var formattedName = HTMLschoolName.replace(data, education.schools[i].name)
+                          .replace("%url%", education.schools[i].url);
       $(".education-entry:last").append(formattedName);
-      var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+      var formattedDegree = HTMLschoolDegree.replace(data, education.schools[i].degree);
       $(".education-entry:last").append(formattedDegree);
-      var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+      var formattedDates = HTMLschoolDates.replace(data, education.schools[i].dates);
       $(".education-entry:last").append(formattedDates);
-      var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+      var formattedLocation = HTMLschoolLocation.replace(data, education.schools[i].city);
       $(".education-entry:last").append(formattedLocation);
-      var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+      var formattedMajor = HTMLschoolMajor.replace(data, education.schools[i].major);
       $(".education-entry:last").append(formattedMajor);
     }
 
-    for (course in education.onlineCourses) {
+    for (var i = 0; i < education.onlineCourses.length; i++) {
       $(".education-entry:last").append(HTMLonlineClasses);
 
-      var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title).replace("%url%", education.onlineCourses[course].url);
+      var formattedTitle = HTMLonlineTitle.replace(data, education.onlineCourses[i].title)
+                          .replace("%url%", education.onlineCourses[i].url);
       $(".education-entry:last").append(formattedTitle);
-      var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+      var formattedSchool = HTMLonlineSchool.replace(data, education.onlineCourses[i].school);
       $(".education-entry:last").append(formattedSchool);
-      var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+      var formattedDates = HTMLonlineDates.replace(data, education.onlineCourses[i].dates);
       $(".education-entry:last").append(formattedDates);
-      var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+      var formattedUrl = HTMLonlineURL.replace(data, education.onlineCourses[i].url);
       $(".education-entry:last").append(formattedUrl);
     }
   }
@@ -145,7 +153,15 @@ var projects = {
     {
     "title" : "Cat ipsum dolor sit amet",
     "dates" : "2016",
-    "description" : "Make muffins hate dog, and attack the dog then pretend like nothing happened for leave hair everywhere. Chase red laser dot spit up on light gray carpet instead of adjacent linoleum knock over christmas tree and make meme, make cute face meow. Curl into a furry donut shake treat bag poop in litter box, scratch the walls find something else more interesting purr for no reason meowzer!. Hopped up on catnip massacre a bird in the living room and then look like the cutest and most innocent animal on the planet.",
+    "description" : "Make muffins hate dog, and attack the dog then pretend like" +
+                    " nothing happened for leave hair everywhere. Chase red laser" +
+                    " dot spit up on light gray carpet instead of adjacent linoleum" +
+                    " knock over christmas tree and make meme, make cute face meow." +
+                    " Curl into a furry donut shake treat bag poop in litter box," +
+                    " scratch the walls find something else more interesting purr" +
+                    " for no reason meowzer!. Hopped up on catnip massacre a bird" +
+                    " in the living room and then look like the cutest and most" +
+                    " innocent animal on the planet.",
     "images" : [
       "http://lorempixel.com/1500/700/cats/3/",
       "http://lorempixel.com/1500/700/cats/9/",
@@ -156,26 +172,25 @@ var projects = {
     }
   ],
   display : function() {
-    for (project in projects.projects) {
+    for (var i = 0; i < projects.projects.length; i++) {
       $("#projects").append(HTMLprojectStart);
 
-      var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+      var formattedTitle = HTMLprojectTitle.replace(data, projects.projects[i].title);
       $(".project-entry:last").append(formattedTitle);
-
-      var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+      var formattedDates = HTMLprojectDates.replace(data, projects.projects[i].dates);
       $(".project-entry:last").append(formattedDates);
-
-      var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+      var formattedDescription = HTMLprojectDescription.replace(data, projects.projects[i].description);
       $(".project-entry:last").append(formattedDescription);
 
-      if (projects.projects[project].images.length > 0) {
+      if (projects.projects[i].images.length > 0) {
         $(".project-entry:last").append('<div id="imagens-projeto" class="carousel carousel-slider">');
-        var i = 0;
+        var n = 0;
         var ordem = ['one!','two!','three!','four!','five!'];
-        for (image in projects.projects[project].images) {
-          var formattedImage = HTMLprojectImage.replace("%ordem%", ordem[i]).replace("%data%", projects.projects[project].images[image]);
+        for (image in projects.projects[i].images) {
+          var formattedImage = HTMLprojectImage.replace("%ordem%", ordem[n])
+                              .replace(data, projects.projects[i].images[n]);
           $("#imagens-projeto:last").append(formattedImage);
-          i++;
+          n++;
         }
         $(".project-entry:last").append('</div>');
       }
